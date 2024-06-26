2b21b435cbc841cefd7ef5a0acd7e80abd6b13c3
haffvieira <haffvieira@sga.pucminas.br>
2024-06-26 18:51:59 -0300

    Update exercicio1_lista6.c

--- Codigo/exercicio1_lista6.c
+++ Codigo/exercicio1_lista6.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-
-int main(void){
-    char nome[31];
-    printf("Digite seu nome (maximo de 30 caracteres): \n");
-    scanf("%[^\n]", nome);
-    printf("Bom dia, %s, \n", nome);
-    return 0;
-}
 
